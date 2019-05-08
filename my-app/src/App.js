583a19b198f2dbf102068c38e89a0640b7cc624b
ConorHoey22 <choey2018@outlook.com>
2019-05-08 18:06:03 +0100

    Files, Bootstrap

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CH Technology Solutions
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+       
+        
+        
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+    <a className= "webServiceTitle">
+          Web Development
+          </a>
+          <br></br>
+          <Button>Click Here</Button>
+    </div>
+    <div class="col-sm">
+    <a className= "GraphicContainer">
+        Graphic Design
         </a>
+        <br></br>
+        <Button>Click here</Button>
+    </div>
+  
+  </div>
+</div>
+
+
+
+
+
+
       </header>
     </div>
   );
